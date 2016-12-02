75033c2c661ca56245fd6a4e68c978d0d3cfa267
21411029 <21411029yi@tama.ac.jp>
2016-12-02 20:39:12 +0900

    棒がスイカに当たると、スイカが非表示になる

---
+++ Assets/CollisionSuika.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionSuika : MonoBehaviour {
+
+	public GameObject suika;
+	public GameObject cube;
+
+
+
+	// Use this for initialization
+	void Start () {
+		cube.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(){
+		suika.SetActive (false);
+		cube.SetActive (true);
+	}
+}
