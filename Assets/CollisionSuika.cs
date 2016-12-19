27fa3e498e06cfcb036d7604d7a7a09430789724
21411029 <21411029yi@tama.ac.jp>
2016-12-19 16:24:04 +0900

    棒がスイカに当たった時、オブジェクトが割れたスイカに変更

--- Assets/CollisionSuika.cs
+++ Assets/CollisionSuika.cs
@@ -4,22 +4,42 @@ using System.Collections;
 public class CollisionSuika : MonoBehaviour {
 
 	public GameObject suika;
-	public GameObject cube;
+	public GameObject gennkeiSuika;
+	public GameObject wareSuika;
+
+
 
 
 
 	// Use this for initialization
 	void Start () {
-		cube.SetActive (false);
+		suika = GameObject.Find ("Suika");
+		gennkeiSuika= GameObject.Find ("gennkei");
+		wareSuika = GameObject.Find ("ware");
+
+		wareSuika.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
-	void OnCollisionEnter(){
-		suika.SetActive (false);
-		cube.SetActive (true);
+	/*void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.name == "suika") {
+			//suika.SetActive (false);
+			suika.GetComponent<MeshRenderer> ().material.color = Color.blue;
+			cube.SetActive (true);
+		}
+	}*/
+	void OnTriggerEnter(Collider other){
+		//Debug.Log ("Hit!");
+		if (other.gameObject.tag == "suika") {
+			//suika.SetActive (false);
+			//suika.GetComponent<MeshRenderer> ().material.color = Color.blue;
+			gennkeiSuika.SetActive (false);
+			wareSuika.SetActive (true);
+
+		}
 	}
 }
