736c3118cba2c37a95a778829982db5306084670
21411029 <21411029yi@tama.ac.jp>
2017-01-31 21:50:07 +0900

    タイトル画面からゲーム画面への遷移実装

---
+++ Assets/ReadyScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReadyScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Text text;
+
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text = GetComponentInChildren<Text> ();
+
+		if (Time.timeSinceLevelLoad >= 1.0f && Time.timeSinceLevelLoad < 2.0f) {
+			text.text = "2";
+		}
+		if (Time.timeSinceLevelLoad >= 2.0f && Time.timeSinceLevelLoad < 3.0f) { 
+			text.text = "1";
+		}
+		if (Time.timeSinceLevelLoad >= 3.0f && Time.timeSinceLevelLoad < 4.0f) {
+			text.text = "start";
+		}
+		if (Time.timeSinceLevelLoad >= 4.0f) {
+			SceneManager.LoadScene ("GetKinect", LoadSceneMode.Single);
+			SceneManager.LoadScene ("suikawari", LoadSceneMode.Additive);
+		}
+	}
+}
