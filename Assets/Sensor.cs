96be9f4eae9de682c3b37112f7c22f88e502e0b2
21411029 <21411029yi@tama.ac.jp>
2017-02-21 16:20:23 +0900

    スイカや棒の座標を受け取るプログラム（仮）作成

---
+++ Assets/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	Vector3 getSuikaPosition (){
+		Vector3 result = new Vector3 (1, 3, 20);
+
+		return result;
+	}
+
+	/*Transform getStickTransform(){
+		Transform result = new Transform ();
+
+		return result;
+	}*/
+
+}
